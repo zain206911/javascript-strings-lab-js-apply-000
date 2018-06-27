5d11277c2ba6fb7285543384c194d258ad1b68dc
Husin Yousif <husinyousif@gmail.com>
2018-06-27 08:21:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+<<<<<<< HEAD
 var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson!"
+=======
+var greeting = "Hello, Everybody!";
+
+var specialGuest = "Hello, Neil deGrasse Tyson"
+>>>>>>> 3e3a518367216bb1ca8f6bc7a82d71082ce39d86
 
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
